59d1e79ad661b62ca85fa5b3a8c1ef1397d97f88
Wesley Ramos <wesley-ramos-de-paula@hotmail.com>
2022-01-30 10:43:03 -0300

    first commit

---
+++ src/main/java/br/com/developertips/mentorship/ApplicationLauncher.java
@@ -0,0 +1,13 @@
+package br.com.developertips.mentorship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLauncher {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationLauncher.class, args);
+	}
+
+}
